00ab92a9208aede25044614748c423f20159afd5
Albert <lidef@yandex.ru>
2019-02-12 01:03:47 +0300

    +

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 Vue.component('good', {
-	template: '#good-template',
-	data: {
-		goods: JSON.parse('productList')
-	}
+	template: '#good-template'
 })
 
 new Vue({
 	el: '#app',
 	data: {
+		productList: []
+	},
+	mounted(){
+		var self = this
+		$.getJSON('products.jsonp', function(data){
+			self.userData = data[0]
+		})
+		console.log(productList);
 	}
 })
