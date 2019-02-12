aa3b681cc5f5eb54d4aaeaf9f0008f5c91133b19
Albert <lidef@yandex.ru>
2019-02-13 00:10:56 +0300

    final

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+var dataURL = 'https://raw.githubusercontent.com/DiceMasters/USStudioTask1/master/products.json'
+
 Vue.component('good', {
-	template: '#good-template'
+	props: ['name', 'description', 'photo', 'price'],
+  template: '#good-template'
 })
 
 new Vue({
 	el: '#app',
-	data: {
-		productList: []
+  data: {
+  	buttonText: 'Без сортировки',
+    sortMode: 'deft',
+		productData: []
 	},
-	mounted(){
-		$.getJSON('products.jsonp', function(data){
-			productList = data[0]
-		})
-		console.log(productList)
+  methods: {
+    sort(data){
+      switch (this.sortMode){
+      	case 'descending':
+        	data.sort( function(obj1, obj2){
+            return obj1.price-obj2.price
+          })
+          this.buttonText = 'По убыванию'
+          this.sortMode = 'ascending'
+        	break
+        case 'ascending':
+        	data.sort( function(obj1, obj2){
+            return obj2.price-obj1.price
+          })
+          this.buttonText = 'По возрастанию'
+          this.sortMode = 'descending'
+        	break
+        default:
+        	data.sort( function(obj1, obj2){
+            return obj1.price-obj2.price
+          })
+          this.buttonText = 'По убыванию'
+          this.sortMode = 'ascending'
+        	break
+      }
+    }
+  },
+	created(){
+  	var self = this
+    
+    fetch(dataURL)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        self.productData = data
+      })
 	}
 })
